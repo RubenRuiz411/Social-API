57ea44a3e95d0f4c68107da81a002e89ce6c3560
RubenRuiz411 <rubenmruiz.411@gmail.com>
2023-09-06 16:30:05 -0700

    fixed files, added index.js to correct folder, removed wrong one

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const userRoutes = require('./user-routes');
-const thoughtRoutes = require('./thought-routes');
-router.use('/users', userRoutes);
-router.use('/thoughts', thoughtRoutes);
+const apiRoutes = require('./api');
+router.use('/api', apiRoutes);
+router.use((req, res) => {
+  res.status(404).send('<h1>Error</h1>');
+});
 module.exports = router;
