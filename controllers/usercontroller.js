80cd3e19e9336d4636c20b904861e0d1c7e97e47
RubenRuiz411 <rubenmruiz.411@gmail.com>
2023-09-09 21:54:34 -0700

    fixed problem with delete user route

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -59,7 +59,7 @@ const userController = {
   deleteUser({ params }, res) {
     Thought.deleteMany({ userId: params.id })
       .then(() => {
-        User.findOneAndDelete({ userId: params.id }).then((dbUserData) => {
+        User.findOneAndDelete({ _id: params.id }).then((dbUserData) => {
           if (!dbUserData) {
             res.status(404).json({ message: 'No User found with this id!' });
             return;
